c1a8bc9a5f13bf9d820fd8c66a84051ce63a9da0
RolandOroz <rolandoroz@gmail.com>
2023-05-02 14:27:14 +0200

    creating pages, and form with input to DB

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from './pages/Books';
+import Add from './pages/Add';
+import Update from './pages/Update';
+import "./style.css";
+//const root = createRoot(document.getElementById("root"));
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>          
+        </Routes>
+      </BrowserRouter>
+     My App
+    </div>
+  );
+}
+
+export default App;
